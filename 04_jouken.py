89f0fdef30f6ba12b7cfdd6bfc0366e200b0bde8
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-07-18 17:28:01 +0900

    追加

---
+++ 04_jouken.py
@@ -0,0 +1,23 @@
+# http://www.pythonweb.jp/tutorial/string/index3.html
+# coding: utf-8
+
+
+hoge = 101
+
+print "hogeは", hoge, "だよ"
+if hoge <= 100:
+	print "100以下だったね"
+else:
+	print "100より大きいね"
+
+
+doubutsu = "dog"
+
+if doubutsu == "cat":
+	print "猫だよ"
+elif doubutsu == "ahiru":
+	print "アヒルだよ"
+else :
+	print "犬でいいんじゃないかな？"
+
+# "elif"なのね

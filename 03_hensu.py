1fa784eb24e0d175d7a2801ff12a21092fbd024b
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-07-18 16:09:05 +0900

    このあたりはrubyとやっぱり変わらんなあ

---
+++ 03_hensu.py
@@ -0,0 +1,19 @@
+# http://www.pythonweb.jp/tutorial/string/index3.html
+# coding: utf-8
+
+#特にrubyと変わらんか
+hoge = 10
+fuga = "hello"
+
+print hoge, fuga
+
+#別のオブジェクト入れても平気
+fuga = 999
+print fuga
+
+#これも普通にある
+fuga += 1
+print fuga
+
+fuga += 1
+print fuga

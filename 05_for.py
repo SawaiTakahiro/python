8253c971623736cd571e9b4938dd34a182117938
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-07-18 17:01:41 +0900

    forとは言ってるけど、Cとかとはちょっと違うのね。 普通にrubyのeachっぽい

---
+++ 05_for.py
@@ -0,0 +1,15 @@
+# http://www.pythonweb.jp/tutorial/string/index3.html
+# coding: utf-8
+
+list = ["ringo", "mikan", "dorayaki"]
+
+for name in list:
+	print name
+
+
+array = [1,10,100,1000,10000]
+for num in array:
+	print num
+
+# オブジェクト.each doっぽく使うにはこんな感じなのか
+# 全部だいたいfor

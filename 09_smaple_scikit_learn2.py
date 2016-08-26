643c1f55e990cad1d883929d2cb5a145c1eadd19
sawaitakahiro <narcissis.jiteki+git@gmail.com>
2016-08-26 10:44:29 +0900

    ようやっと意味わかった。これでtreeは使えるかもしれない1

--- 09_smaple_scikit_learn2.py
+++ 09_smaple_scikit_learn2.py
@@ -4,14 +4,14 @@ import numpy as np
 
 from sklearn import tree
 
-hoge = np.array([[1,2,3],[200,300,400],[5,6,8]])
-fuga = np.array([[0,1,2],[2,3,4],[5,6,7]])
+hoge = np.array([[1,2,3],[200,300,400],[5,6,8],[800,900,1000]])
+fuga = np.array([0,1,2,3])
 
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(hoge, fuga)
 
-test_data = ([[200,300,400]])
+test_data = ([[1,900,1000]])
 
 result = clf.predict(test_data)
 print(result)

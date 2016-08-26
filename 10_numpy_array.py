2ac30fe6a170e939b7c505cadd0e994370f8a7e6
sawaitakahiro <narcissis.jiteki+git@gmail.com>
2016-08-26 10:19:07 +0900

    numpyのarray。通常のリストとは違うっぽい。

---
+++ 10_numpy_array.py
@@ -0,0 +1,12 @@
+#http://ibisforest.org/index.php?python%2Fnumpy より
+# -*- coding: utf-8 -*-
+import numpy as np
+
+hoge = np.array([2,3,4])	#で１次元配列
+fuga = np.array([[3,4],[8,10],[13,16]])	#で二次元配列
+
+print(hoge)
+print(fuga)
+
+
+
